8417489656d4801f32a76f298b4031d83393011b
Eben Maglic <eben.maglic@gmail.com>
2025-04-22 14:10:26 -0700

    updated construct-viewr-url.js so it works with development and production urls

--- src/utils/construct-viewer-url.js
+++ src/utils/construct-viewer-url.js
@@ -1,7 +1,11 @@
 import { utf8ToBase64, base64ToUtf8 } from "./base64Converter";
 
 const constructViewerURL = ({ html, css, js }) => {
-  const url = `${window.location.origin}/#/viewer/?html=${utf8ToBase64(
+  const origin =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://eben.design/static/tools/jsbx2";
+  const url = `${origin}/#/viewer/?html=${utf8ToBase64(
     html
   )}&css=${utf8ToBase64(css)}&js=${utf8ToBase64(js)}`;
   return url;
